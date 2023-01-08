627074e64f535e1e4dc8bf95c6265d89c90ba255
Pooya <alyxwhite69@gmail.com>
2023-01-08 19:27:07 +0330

    base url changed

--- Weblog_Django/urls.py
+++ Weblog_Django/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 from django.conf.urls import url
 
 urlpatterns = [
+    url(r'^$', include('posts.urls')),
     path('admin/', admin.site.urls),
     path('posts/', include('posts.urls')),
 ]
