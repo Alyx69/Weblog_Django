b5b55e9fa0954b0e88ad74fab05f07889b6620f8
Pooya <alyxwhite69@gmail.com>
2023-01-08 19:01:41 +0330

    urls.py and views.py changed

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Posts
 
 # Create your views here.
+def index(request):
+
+    return render(request, 'posts/index.html', context)
